7cf9c912ba051f6a2f9b1d20fba1cc8c4067edd0
Alexander Cohen <Alexcohen.is@gmail.com>
2020-03-04 20:08:27 +1000

    Added Contact Scaffold.

---
+++ db/migrate/20200304100646_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.belongs_to :company, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
