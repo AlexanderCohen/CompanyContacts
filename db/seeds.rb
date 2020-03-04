46af6dbe5d9845a5c6d88ce5d908e745330c315f
Alexander Cohen <Alexcohen.is@gmail.com>
2020-03-04 20:51:00 +1000

    Added a cheeky seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+new_company = Company.create(name:"Two Red Kites - Take away", abn: "1234223")
+first_contact = Contact.create(name:"Nigel", company_id:1)
+second_contact = Contact.create(name:"Geoff", company_id:1)
